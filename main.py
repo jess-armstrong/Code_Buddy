cef18ae0b284bcdce3c1fed102db38928d04abcd
jess-armstrong <jessfarmstrong@gmail.com>
2025-06-24 17:02:19 -0400

    add message list to keep track of conversation
    add support for --verbose flag

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
 
-client = genai.Client(api_key=api_key)
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
-if len(sys.argv) < 2:
-    print("Usage: python3 main.py <prompt>")
-    sys.exit(1)
+    if not args:
+        print("Usage: python main.py <prompt>")
+        sys.exit(1)
 
-response = client.models.generate_content(model = 'gemini-2.0-flash-001', contents = sys.argv[1])
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_response(client, messages, verbose, user_prompt)
+
+
+def generate_response(client, messages, verbose, user_prompt):
+    response = client.models.generate_content(
+        model = 'gemini-2.0-flash-001', 
+        contents = messages
+    )
+    print(response.text)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
