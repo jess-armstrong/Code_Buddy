c79ea7ac6b49886cc88803ff5f32331d05e18e74
jess-armstrong <jessfarmstrong@gmail.com>
2025-06-30 14:29:00 -0400

    allow gemini to call functions

--- call_function.py
+++ call_function.py
@@ -1,8 +1,10 @@
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.write_file import schema_write_file
-from functions.run_python_file import schema_run_python_file
+
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.write_file import write_file, schema_write_file
+from functions.run_python_file import run_python_file, schema_run_python_file
+from config import WORKING_DIR
 
 available_functions = types.Tool(
         function_declarations=[
@@ -12,3 +14,42 @@ available_functions = types.Tool(
             schema_run_python_file
         ]
     )
+
+def call_function(function_call_part, verbose = False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f"Calling function: {function_call_part.name}")
+
+    function_dict = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "write_file" : write_file,
+        "run_python_file" : run_python_file,
+    }
+
+    func_name = function_call_part.name
+    if func_name not in function_dict:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name = func_name,
+                    response={"error": f"Unknown function: {func_name}"},
+                )
+            ]
+        )
+    
+    args = dict(function_call_part.args)
+    args["working_directory"] = WORKING_DIR
+    func_result = function_dict[func_name](**args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+            name = func_name,
+            response={"result": func_result},
+        )
+    ],
+)
+    
--- main.py
+++ main.py
@@ -4,19 +4,8 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
-import logging
-
-# class _NoFunctionCallWarning(logging.Filter):
-#     def filter(self, record: logging.LogRecord) -> bool:
-#         message = record.getMessage()
-#         if "there are non-text parts in the response:" in message:
-#             return False
-#         else:
-#             return True
-
-# logging.getLogger("google_genai.types").addFilter(_NoFunctionCallWarning())
 
 def main():
     load_dotenv()
@@ -43,6 +32,9 @@ def main():
 
 def generate_response(client, messages, verbose, user_prompt):
 
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+
     response = client.models.generate_content(
         model = 'gemini-2.0-flash-001', 
         contents = messages,
@@ -51,21 +43,30 @@ def generate_response(client, messages, verbose, user_prompt):
             system_instruction=system_prompt
         )
     )
+   
+    if not response.function_calls:
+        print(response.text) 
+        return response.text
+    
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("Empty function call result")
+        if verbose:
+            print(f'-> {function_call_result.parts[0].function_response.response["result"]}')
+        function_responses.append(function_call_result.parts[0])
 
+    if not function_responses:
+        raise Exception("No function responses generated, exiting.")
+    
     if verbose:
-        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-    function_calls = response.function_calls
-
-    if function_calls:
-        for function_call_part in function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-        
-    else: 
-        print(response.text)
     
+ 
 
 
 if __name__ == "__main__":
